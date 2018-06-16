eb7b0dcf0fcff22dd90fb9e1c25417c6ad283ece
AlfieriChou <alfierichou@look.com>
2018-06-17 06:23:29 +0800

    json message

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello World")
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
 	})
 	r.Run()
 }
