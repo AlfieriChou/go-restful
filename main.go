2d4269a4b018000eaaccc04384afcbd54051523a
AlfieriChou <alfierichou@look.com>
2018-06-17 07:07:46 +0800

    gorm

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+  	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func main() {
+	db, _ := gorm.Open("mysql", "root:@test?charset=utf-8&parseTime=True&loc=Local")
+	defer db.Close()
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
