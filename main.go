5589d17d6d9cf9009b1d08c1859b7b11a0ad5d20
AlfieriChou <alfierichou@look.com>
2018-06-17 00:06:07 +0800

    add gin

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello World")
+	})
 }
