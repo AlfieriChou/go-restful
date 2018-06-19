c3e76969af3a8ad6a450668583ad3d616345ff83
AlfieriChou <alfierichou@look.com>
2018-06-19 17:26:25 +0800

    get data

--- main.go
+++ main.go
@@ -1,20 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
-  	_ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+var db *gorm.DB
+var err error
+
+type Person struct {
+	ID        uint   `json:"id"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
 func main() {
-	db, _ := gorm.Open("mysql", "root:@test?charset=utf-8&parseTime=True&loc=Local")
-	defer db.Close()
+
+	db, err := gorm.Open("mysql", "root:@(127.0.0.1:3306)/test?charset=utf8&parseTime=True&loc=Local")
+
+    if err != nil {
+        fmt.Println(err)
+        panic("--- 数据库连接失败")
+    }
+    defer db.Close()
+
+	db.AutoMigrate(&Person{})
 
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
-	r.Run()
+	r.GET("/", GetProjects)
+
+	r.Run(":8080")
+}
+
+func GetProjects(c *gin.Context) {
+	var person []Person
+	if err := db.Find(&person).Error; err != nil {
+		c.AbortWithStatus(404)
+		fmt.Println(err)
+	} else {
+		c.JSON(200, person)
+	}
 }
